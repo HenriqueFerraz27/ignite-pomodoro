c093b8c0d4fa475242412be956c29786fae9f97d
Henrique Ferraz <henriqueferrazsilva27@gmail.com>
2023-11-12 20:38:25 -0800

    🚧feat(page): home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,33 @@
+import * as Icon from 'phosphor-react'
+
 export const Home = () => {
-  return <h1>Home</h1>
+  return (
+    <>
+      <form action="">
+        <div>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" type="text" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input id="minutesAmount" type="number" />
+
+          <span>minutos.</span>
+        </div>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+
+        <button type="submit">
+          <Icon.Play /> Começar
+        </button>
+      </form>
+    </>
+  )
 }
 
 export default Home
