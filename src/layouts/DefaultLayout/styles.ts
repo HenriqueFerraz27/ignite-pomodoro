e12d88550455f0fc017c436c27616b99a8f6bfac
Henrique Ferraz <henriqueferrazsilva27@gmail.com>
2023-11-14 19:14:39 -0800

    💄style(layout): default layout - min height

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -5,7 +5,7 @@ export const DefaultLayout = styled.main`
   display: flex;
   flex-direction: column;
   max-width: 75%;
-  height: calc(100vh - ${pxToRem(160)});
+  min-height: calc(100vh - ${pxToRem(160)});
   margin: ${pxToRem(80)} auto;
   background-color: ${({ theme }) => theme.colors.base.shape.tertiary};
   border-radius: ${pxToRem(8)};
