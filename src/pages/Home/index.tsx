9601c574cb45bd0ff255ef016609f5f3c13c29c6
Henrique Ferraz <henriqueferrazsilva27@gmail.com>
2023-11-19 23:59:00 -0800

    🏗feat(hook-form): form management

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,28 @@
 import * as Icon from 'phosphor-react'
 import * as S from './styles'
+import { useForm } from 'react-hook-form'
 
 export const Home = () => {
+  const { register, handleSubmit, watch } = useForm()
+
+  const handleCreateNewCycle = (data: any) => {
+    console.log(data)
+  }
+
+  const task = watch('task')
+  const minutesAmount = watch('minutesAmount')
+  const isSubmitDisable = !task || !minutesAmount
+
   return (
     <S.Home>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <S.FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <S.TaskInput
             id="task"
             type="text"
             placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
           />
 
           <label htmlFor="minutesAmount">durante</label>
@@ -21,6 +33,7 @@ export const Home = () => {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -34,7 +47,7 @@ export const Home = () => {
           <span>0</span>
         </S.Pomodoro>
 
-        <S.PomodoroButton type="submit">
+        <S.PomodoroButton type="submit" disabled={isSubmitDisable}>
           <Icon.Play weight="bold" /> Começar
         </S.PomodoroButton>
       </form>
