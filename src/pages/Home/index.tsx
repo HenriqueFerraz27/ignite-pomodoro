f5cd6a157b376f69fe092d00e942e45578fae234
Henrique Ferraz <henriqueferrazsilva27@gmail.com>
2023-11-21 22:59:59 -0800

    🏗feat(datalist): task suggestion

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -21,9 +21,18 @@ export const Home = () => {
           <S.TaskInput
             id="task"
             type="text"
+            list="task-suggestion"
             placeholder="Dê um nome para o seu projeto"
             {...register('task')}
           />
+
+          <datalist id="task-suggestion">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+            <option value="Projeto 5" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <S.MinutesAmountInput
