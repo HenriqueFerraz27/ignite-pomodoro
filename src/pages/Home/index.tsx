7cd38e0ad8e913e0a37fe2668a3be4756a6507be
Henrique Ferraz <henriqueferrazsilva27@gmail.com>
2023-12-05 23:11:05 -0800

    🏗feat: creating countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -24,6 +24,7 @@ interface Cycle {
 export const Home = () => {
   const [cycles, setCycles] = useState<Cycle[]>([])
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -46,11 +47,18 @@ export const Home = () => {
 
   const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
 
-  console.log(activeCycle)
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+  const minutesAmount = Math.floor(currentSeconds / 60)
+  const secondsAmount = currentSeconds % 60
+
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
 
   const task = watch('task')
-  const minutesAmount = watch('minutesAmount')
-  const isSubmitDisable = !task || !minutesAmount
+  const minutesAmountInput = watch('minutesAmount')
+  const isSubmitDisable = !task || !minutesAmountInput
 
   return (
     <S.Home>
@@ -88,11 +96,11 @@ export const Home = () => {
         </S.FormContainer>
 
         <S.Pomodoro>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <S.Separator>:</S.Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
         </S.Pomodoro>
 
         <S.PomodoroButton type="submit" disabled={isSubmitDisable}>
